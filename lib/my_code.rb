89599fe76cb2e6601bfca753d60d5a3e51875996
Li Xie <li_xie1992@hotmail.com>
2019-09-09 22:41:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(array)
+  
+  new_array = []
+  
+  array.length.times do |i|
+    new_array.push yield(array[i])
+  end
+  
+  new_array
+end 
+
+def reduce(array, start = nil)
+  
+  if start
+    new_value = start 
+    i = 0 
+  else
+    new_value = array[0]
+    i = 1 
+  end 
+  
+  while i < array.length
+    new_value = yield(new_value, array[i])
+    i += 1
+  end 
+  
+  new_value
+end
