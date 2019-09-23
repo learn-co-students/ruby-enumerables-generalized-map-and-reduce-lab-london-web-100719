d973c0ded6914378e208ab4a3fce78dbd433ddf2
Dominc Carolan <dominiccarolan@gmail.com>
2019-09-23 17:24:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(array)
+  new_array = []
+  counter = 0
+  while counter < array.length do
+   new_array[counter] = yield(array[counter])
+    counter += 1
+  end
+  new_array
+end 
+
+def reduce(array, start_point = nil)
+  counter = 0
+  if start_point 
+    my_return = start_point
+  else 
+    my_return = array[0]
+    counter += 1 
+  end
+  while counter < array.length do
+    my_return = yield(my_return, array[counter])
+    counter += 1 
+  end
+  my_return
+end
